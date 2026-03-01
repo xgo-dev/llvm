cf30809de9ec45922ffc3086030909643edfac3a
ZhouGuangyuan <zhouguangyuan.xian@gmail.com>
2026-03-01 10:52:34 +0800

    Keep LLVM 19 as the default version

--- llvm_config_darwin_llvm19.go
+++ llvm_config_darwin_llvm19.go
@@ -1,4 +1,4 @@
-//go:build !byollvm && darwin && !llvm14 && !llvm15 && !llvm16 && !llvm17 && !llvm18 && !llvm20
+//go:build !byollvm && darwin && !llvm14 && !llvm15 && !llvm16 && !llvm17 && !llvm18 && !llvm20 && !llvm21
 
 package llvm
 
